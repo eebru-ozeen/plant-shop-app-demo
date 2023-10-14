6d30c7c8720b48fb3dd4dbf31db25f62f62b9114
Ebru Beyza Özen <eebru.ozeen@gmail.com>
2023-10-14 06:10:50 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+//rafce from redux snippet extension
+//arrow function component
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>Hello Plant Shopers</h1>
+  )
+}
+
+export default App
